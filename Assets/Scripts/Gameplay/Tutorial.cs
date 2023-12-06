5e50659e943cb80a0a3f532742edb569deb0aee3
Roderick Montague <roderick.montague243@gmail.com>
2023-12-05 23:07:55 -0500

    Finished Updating Tutorial Text + Added TTSOnEnable
    The tutorial text has been updated. I've also added a script to trigger TTS when an object is activated (uses 'OnEnable()') to trigger some TTS points.

--- Assets/Scripts/Gameplay/Tutorial.cs
+++ Assets/Scripts/Gameplay/Tutorial.cs
@@ -42,7 +42,7 @@ namespace RM_EM
         private JSONNode defs;
 
         [Header("Triggers")]
-        
+
         // TODO: maybe seperate them based on the tutorial type (world, match). Maybe re-arrange the function.
         // Bools for clearing certain tutorials.
         public bool clearedOpening = false;
@@ -161,52 +161,25 @@ namespace RM_EM
             {
                 pages.Add(new Page(defs["trl_opening_00"], "trl_opening_00"));
                 pages.Add(new Page(defs["trl_opening_01"], "trl_opening_01"));
+                pages.Add(new Page(defs["trl_opening_02"], "trl_opening_02"));
+                pages.Add(new Page(defs["trl_opening_03"], "trl_opening_03"));
+                pages.Add(new Page(defs["trl_opening_04"], "trl_opening_04"));
             }
             else
             {
-                pages.Add(new Page("Welcome to the exponent club! As the name suggests, we play the exponent game here. Do you want to play? Oh, you’ve never played the game before? I guess the club will have to teach you then! Challenge me to a match, and I’ll show you the ropes!"));
-                pages.Add(new Page("The exponent game is simple. You and your opponent will both be given equations with missing values, which can be numbers or operations. You need to pick the right value from your puzzle board to complete the equation. When the equation is complete, you get points, and move onto the next equation. The player fills their points bar first wins. With all that explained, challenge me to a match by selecting my icon!"));
+                pages.Add(new Page("Welcome to the Exponent Club! I’m Minraul, and as you can guess, we play the exponent game here! Oh, you’ve never played the game before? Well, I guess I’ll just have to teach you then!"));
+                pages.Add(new Page("The exponent game is simple. You answer exponent questions as fast as you can to gain points and reach the goal before your opponent. The questions all have missing values, which you need to fill in correctly and in the right order."));
+                pages.Add(new Page("The game will give you values to choose from to try and fill in the empty slot. The way this is handled can vary by match, but they’re all self-explanatory. Outside of selecting values, you can also use skips and powers."));
+                pages.Add(new Page("Skips allow you to skip your current question and get a new one. Naturally, this means you get no points since you didn’t finish the question. Skipping also has a cooldown to it, so it’s best not to use it constantly."));
+                pages.Add(new Page("As for powers, I’ll talk more about them later. With all that explained, issue a challenge to me to start your first match! You can do so by clicking on my icon in the world area."));
             }
-            
+
 
             clearedOpening = true;
 
             return pages;
         }
 
-        // The exponent basics tutorial.
-        public List<Page> GetExponentTutorial()
-        {
-            // Pages
-            List<Page> pages = new List<Page>();
-
-            // Loads the pages
-            if (defs != null) // Translation
-            {
-                pages.Add(new Page(defs["trl_exponent_00"], "trl_exponent_00"));
-            }
-            else
-            {
-                pages.Add(new Page("Time to teach you some exponent basics. Exponents are math operations where you multiply a value by itself a certain number of times, with said number being determined by the exponent. Exponent operations have multiple rules, which will be explained later."));
-            }
-
-
-            // If the world manager is instantiated.
-            if(WorldManager.Instantiated)
-            {
-                // Gets the instance.
-                WorldManager manager = WorldManager.Instance;
-
-                // Makes the info button interactable.
-                pages[0].OnPageClosedAddCallback(manager.worldUI.SetInfoButtonInteractable);
-            }
-            
-
-            // Cleared the tutorial.
-            clearedExponent = true;
-
-            return pages;
-        }
 
         // The first match loss tutorial.
         public List<Page> GetFirstMatchLossTutorial()
@@ -221,7 +194,7 @@ namespace RM_EM
             }
             else
             {
-                pages.Add(new Page("You’ve lost the introductory match. It’s important that you learn this rule before going forward, so please try again."));
+                pages.Add(new Page("You lost the match. You need to learn the basics before you can take on the other club members, so please try again."));
             }
 
             clearedFirstMatchLoss = true;
@@ -239,118 +212,15 @@ namespace RM_EM
             if (defs != null) // Translation
             {
                 pages.Add(new Page(defs["trl_firstMatchWin_00"], "trl_firstMatchWin_00"));
+                pages.Add(new Page(defs["trl_firstMatchWin_01"], "trl_firstMatchWin_01"));
             }
             else
             {
-                pages.Add(new Page("Congratulations on winning the introduction match! Now that you’re familiar with the rules, you’ll have to beat every member of the club! Each member focuses on one or more exponent rules, so you’ll get introduced to each rule one at a time. You’ll be a master at this in no time. Good luck!"));
+                pages.Add(new Page("You won your first match! Now that you’re familiar with the rules, you can take on the other club members. Some of them won’t accept your challenge until you become more experienced, so your options are restricted."));
+                pages.Add(new Page("Some club members utilize exponent rules that you haven’t learned yet. When you encounter a new rule, I’ll be there to explain it. Feel free to check the ‘info menu’ as well if you ever need a refresher. Good luck!"));
             }
 
             clearedFirstMatchWin = true;
-
-            return pages;
-        }
-
-        // The product rule tutorial.
-        public List<Page> GetProductRuleTutorial()
-        {
-            // The pages list.
-            List<Page> pages = new List<Page>();
-
-            // Loads the pages
-            if (defs != null) // Translation
-            {
-                pages.Add(new Page(defs["trl_product_00"], "trl_product_00"));
-            }
-            else
-            {
-                pages.Add(new Page("This match uses the product rule. When two terms with the same base are multiplied together, it can be simplified to multiplying the base by the sum of the exponents."));
-            }
-
-            clearedProduct = true;
-
-            return pages;
-        }
-
-        // The power of a power rule tutorial.
-        public List<Page> GetPowerOfAPowerRuleTutorial()
-        {
-            // The pages list.
-            List<Page> pages = new List<Page>();
-
-            // Loads the pages
-            if (defs != null) // Translation
-            {
-                pages.Add(new Page(defs["trl_powerOfAPower_00"], "trl_powerOfAPower_00"));
-            }
-            else
-            {
-                pages.Add(new Page("This match uses the power of a power rule. When a term with an exponent is having its result get another exponent applied to it, it can be converted to the base value to a single, combined exponent. The combined exponent is the two exponents from the original expression multiplied together."));
-            }
-
-            clearedPowerOfAPower = true;
-
-            return pages;
-        }
-
-        // The product of a product rule tutorial.
-        public List<Page> GetPowerOfAProductRuleTutorial()
-        {
-            // The pages list.
-            List<Page> pages = new List<Page>();
-
-            // Loads the pages
-            if (defs != null) // Translation
-            {
-                pages.Add(new Page(defs["trl_powerOfAProduct_00"], "trl_powerOfAProduct_00"));
-            }
-            else
-            {
-                pages.Add(new Page("This match uses the product of a product rule. If two terms with different bases and the same exponents are multiplied, the bases are multiplied together, with their result being put to the power of the shared exponent."));
-            }
-
-            clearedPowerOfAProduct = true;
-
-            return pages;
-        }
-
-        // The zero exponent rule tutorial.
-        public List<Page> GetZeroExponentRuleTutorial()
-        {
-            // The pages list.
-            List<Page> pages = new List<Page>();
-
-            // Loads the pages
-            if (defs != null) // Translation
-            {
-                pages.Add(new Page(defs["trl_zero_00"], "trl_zero_00"));
-            }
-            else
-            {
-                pages.Add(new Page("This match uses the zero exponent rule. Any value to the power of 0 will give the result of 1."));
-            }
-
-            clearedZero = true;
-
-            return pages;
-        }
-
-        // The negative exponent rule tutorial.
-        public List<Page> GetNegativeExponentRuleTutorial()
-        {
-            // The pages list.
-            List<Page> pages = new List<Page>();
-
-            // Loads the pages
-            if (defs != null) // Translation
-            {
-                pages.Add(new Page(defs["trl_negative_00"], "trl_negative_00"));
-            }
-            else
-            {
-                pages.Add(new Page("This match uses the negative exponent rule. The negative exponent rule dictates that any value to the power of a negative exponent becomes a fraction with a numerator of 1. The value with the exponent applied becomes the denominator."));
-            }
-
-            clearedNegative = true;
 
             return pages;
         }
@@ -365,10 +235,14 @@ namespace RM_EM
             if (defs != null) // Translation
             {
                 pages.Add(new Page(defs["trl_firstPower_00"], "trl_firstPower_00"));
+                pages.Add(new Page(defs["trl_firstPower_01"], "trl_firstPower_01"));
+                pages.Add(new Page(defs["trl_firstPower_02"], "trl_firstPower_02"));
             }
             else
             {
-                pages.Add(new Page("You just got a match power! A power can be used to give you an edge during a match, but only after building up its energy first. When you defeat a challenger, you also receive the power, which you can equip for future matches. Check the power menu to see what each power does, and select what power you want."));
+                pages.Add(new Page("You just got a match power! A match power is a power-up that can be used during a match, but only after you charge it up. Powers charge up when you answer questions correctly, so just playing the game like usual is enough."));
+                pages.Add(new Page("After a power is fully charged, you can activate it with the ‘power button’ at anytime. The power’s effects remain active until the power’s energy runs out, after-which you need to charge it up again."));
+                pages.Add(new Page("Unlocked powers can be viewed and equipped in the ‘powers menu’. Your opponents can use powers too, so keep that in mind. Good luck!"));
             }
 
             clearedFirstPower = true;
@@ -389,7 +263,7 @@ namespace RM_EM
             }
             else
             {
-                pages.Add(new Page("You’ve learned all the exponent rules and defeated all the other club members, so now it’s time for your final challenge: a match against me! Head over to the next room so we can face off!"));
+                pages.Add(new Page("You’ve learned all the exponent rules and defeated all the other club members, so now it’s time for your final match! A rematch against me! Select my icon in the next area to challenge me like before."));
             }
 
             clearedFinalMatch = true;
@@ -410,7 +284,7 @@ namespace RM_EM
             }
             else
             {
-                pages.Add(new Page("You got a game over. You can rematch your opponent now, or try to take on any other available challengers. However, you’ll need to beat all the club members to clear this trial, so be ready to come back at some point."));
+                pages.Add(new Page("You lost the match. You can rematch the opponent now or return to the world area. Every club member must be defeated for you to complete the challenge, but you can take as many tries as you like."));
             }
 
             clearedGameOver = true;
@@ -419,6 +293,149 @@ namespace RM_EM
         }
 
 
+
+        // EXPONENTS //
+        // The exponent basics tutorial.
+        public List<Page> GetExponentTutorial()
+        {
+            // Pages
+            List<Page> pages = new List<Page>();
+
+            // Loads the pages
+            if (defs != null) // Translation
+            {
+                pages.Add(new Page(defs["trl_exponent_00"], "trl_exponent_00"));
+            }
+            else
+            {
+                pages.Add(new Page("This match only uses exponent basics. Exponents are math operations that multiply a base by itself a certain number of times. Check the info window if you ever want a refresh on the exponent rules."));
+            }
+
+
+            // If the world manager is instantiated.
+            if (WorldManager.Instantiated)
+            {
+                // Gets the instance.
+                WorldManager manager = WorldManager.Instance;
+
+                // Makes the info button interactable.
+                pages[0].OnPageClosedAddCallback(manager.worldUI.SetInfoButtonInteractable);
+            }
+
+
+            // Cleared the tutorial.
+            clearedExponent = true;
+
+            return pages;
+        }
+
+        // The product rule tutorial.
+        public List<Page> GetProductRuleTutorial()
+        {
+            // The pages list.
+            List<Page> pages = new List<Page>();
+
+            // Loads the pages
+            if (defs != null) // Translation
+            {
+                pages.Add(new Page(defs["trl_product_00"], "trl_product_00"));
+            }
+            else
+            {
+                pages.Add(new Page("This match uses the product rule. When the same bases with different exponents are multiplied together, the exponents can be summed together and applied to said base to simplify the operation."));
+            }
+
+            clearedProduct = true;
+
+            return pages;
+        }
+
+        // The power of a power rule tutorial.
+        public List<Page> GetPowerOfAPowerRuleTutorial()
+        {
+            // The pages list.
+            List<Page> pages = new List<Page>();
+
+            // Loads the pages
+            if (defs != null) // Translation
+            {
+                pages.Add(new Page(defs["trl_powerOfAPower_00"], "trl_powerOfAPower_00"));
+            }
+            else
+            {
+                pages.Add(new Page("This match uses the power of a power rule. If a base is having two exponents applied to it, then said exponents can be multiplied together, with the base being raised to the product of that operation."));
+            }
+
+            clearedPowerOfAPower = true;
+
+            return pages;
+        }
+
+        // The product of a product rule tutorial.
+        public List<Page> GetPowerOfAProductRuleTutorial()
+        {
+            // The pages list.
+            List<Page> pages = new List<Page>();
+
+            // Loads the pages
+            if (defs != null) // Translation
+            {
+                pages.Add(new Page(defs["trl_powerOfAProduct_00"], "trl_powerOfAProduct_00"));
+            }
+            else
+            {
+                pages.Add(new Page("This match uses the power of a product rule. If two bases with the same exponents are multiplied together, it can be simplified to the multiplication of said bases, with the result being raised to the shared exponent."));
+            }
+
+            clearedPowerOfAProduct = true;
+
+            return pages;
+        }
+
+        // The zero exponent rule tutorial.
+        public List<Page> GetZeroExponentRuleTutorial()
+        {
+            // The pages list.
+            List<Page> pages = new List<Page>();
+
+            // Loads the pages
+            if (defs != null) // Translation
+            {
+                pages.Add(new Page(defs["trl_zero_00"], "trl_zero_00"));
+            }
+            else
+            {
+                pages.Add(new Page("This match uses the zero rule. Any number raised to the exponent 0 will return a result of 1."));
+            }
+
+            clearedZero = true;
+
+            return pages;
+        }
+
+        // The negative exponent rule tutorial.
+        public List<Page> GetNegativeExponentRuleTutorial()
+        {
+            // The pages list.
+            List<Page> pages = new List<Page>();
+
+            // Loads the pages
+            if (defs != null) // Translation
+            {
+                pages.Add(new Page(defs["trl_negative_00"], "trl_negative_00"));
+            }
+            else
+            {
+                pages.Add(new Page("This match uses the negative rule. A number raised to a negative exponent will become its reciprocal, and have the exponent be applied to the denominator."));
+            }
+
+            clearedNegative = true;
+
+            return pages;
+        }
+
+
+        // OTHER //
         // Generates tutorial data.
         public TutorialData GenerateTutorialData()
         {
@@ -442,7 +459,7 @@ namespace RM_EM
             data.clearedNegative = clearedNegative;
 
             return data;
-    }
+        }
 
         // Loads tutorial data.
         public void LoadTutorialData(TutorialData data)
