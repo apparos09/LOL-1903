66be5a41faef0b0146fea7699a6e814af7532f00
Roderick Montague <roderick.montague243@gmail.com>
2023-12-27 14:08:07 -0500

    Deleting Custom Text Scaler
    It turns out Unity has built-in text scaling options, so I'm deleting the custom text scaler I was making.

--- Assets/Scripts/TMP_TextScaler.cs
+++ Assets/Scripts/TMP_TextScaler.cs
@@ -16,21 +16,21 @@ namespace RM_EM
         // Width
         public float textWidth = 1;
 
-        // Height
-        public float textHeight = 1;
+        //// Height
+        //public float textHeight = 1;
 
         [Header("Settings")]
         // Adjust the width.
         public bool adjustWidth = true;
 
-        // Adjust the height.
-        public bool adjustHeight = true;
+        // // Adjust the height.
+        // public bool adjustHeight = true;
 
         // Start is called before the first frame update
         void Start()
         {
             // Word wrapping should be disabled.
-            // text.enableWordWrapping = false;
+            text.enableWordWrapping = false;
         }
 
         // Scales the text (TODO: implement)
@@ -44,8 +44,8 @@ namespace RM_EM
             float areaLeft = text.transform.position.x - (textWidth / 2);
             float areaRight = text.transform.position.x + (textWidth / 2);
 
-            float areaTop = text.transform.position.y + (textHeight / 2);
-            float areaBottom = text.transform.position.y - (textHeight / 2);
+            //float areaTop = text.transform.position.y + (textHeight / 2);
+            //float areaBottom = text.transform.position.y - (textHeight / 2);
 
             // TEXT
             // Gets the text info of the text object.
@@ -60,16 +60,17 @@ namespace RM_EM
             {
                 // TODO: scale
             }
-
-            // Scale Y
-            if (tmpCharInfoFirst.topLeft.y > areaTop || tmpCharInfoLast.bottomLeft.x < areaBottom)
+            else
             {
-                // TODO: scale
+                text.transform.localScale = Vector3.one;
             }
 
+            //// Scale Y
+            //if (tmpCharInfoFirst.topLeft.y > areaTop || tmpCharInfoLast.bottomLeft.x < areaBottom)
+            //{
+            //    // TODO: scale
+            //}
 
-            // float bw = buttonWidth * button.transform.localScale.x;
-            // float bh = buttonHeight * button.transform.localScale.y;
 
         }
 
