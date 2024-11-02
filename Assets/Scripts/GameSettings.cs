237df50c834f28eb8c05b38972191090e531eae7
mecha-rm <roderick.montague243@gmail.com>
2024-11-02 11:36:32 -0400

    Updated Utils Files
    Some utility files have been updated to provide some fixes for the game.

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -27,6 +27,10 @@ namespace RM_EM
         // This is only relevant when starting up the game scene.
         private bool useTutorial = true; // Off by default to help with game scene testing.
 
+        // The global volume, which is used for muting/unmuting the entire game.
+        // [HideInInspector] // Not needed since this is a private variable.
+        private float globalVolume = 1.0F;
+
         // Audio Tags
         // The tag for BGM objects.
         public const string BGM_TAG = "BGM";
@@ -185,14 +189,50 @@ namespace RM_EM
         {
             get
             {
-                // is audio muted.
-                return AudioListener.pause;
+                // Checks if the audio is muted.
+                // return AudioListener.pause; // Old
+
+                // New
+                // Checks if the global volume is less than or equal to 0.
+                // If it is, then the game is muted.
+                if (AudioListener.volume <= 0.0F)
+                {
+                    return true;
+                }
+                else
+                {
+                    // Updates the global volume to make sure it matches the set volume.
+                    globalVolume = AudioListener.volume;
+                    return false;
+                }
             }
 
             set
             {
-                // mutes/unmutes all audio.
-                AudioListener.pause = value;
+                // NOTE: using AudioListener.pause still stores the audio play calls.
+                // When the AudioListener is unpaused, those stored sound effects end up playing.
+                // As such, global volume is used instead of AudioListener.pause.
+
+                // Mutes/unmutes all audio.
+                // Old: uses AudioListener.pause.
+                // AudioListener.pause = value;
+
+                // New: uses AudioListener.volume.
+                if (value) // Mute the game.
+                {
+                    // Gets the AudioListener's volume, and sets it to 0.
+                    globalVolume = AudioListener.volume;
+                    AudioListener.volume = 0.0F;
+                }
+                else // Unmute the game.
+                {
+                    // If the global volume is 0, set it to 1.
+                    if (globalVolume <= 0.0F)
+                        globalVolume = 1.0F;
+
+                    // Sets the audio listener's volume to the global volume.
+                    AudioListener.volume = globalVolume;
+                }
             }
         }
 
@@ -380,6 +420,7 @@ namespace RM_EM
             AdjustAllAudioLevels();
 
             // Refreshes the game mute, since this caused problems before.
+            // This shouldn't be necessary anymore, but it's being left in.
             Mute = Mute;
         }
 
